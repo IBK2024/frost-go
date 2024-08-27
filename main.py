e483897788f760282991e3153a8113154d76fa37
IBK <ibukunoluwa.adetoba@gmail.com>
2024-08-27 20:23:46 +0100

    Fastapi setup

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+import uvicorn
+import asyncio
+from run import app
+
+
+async def main() -> None:
+    config = uvicorn.Config(app, port=5000, log_level="info", reload=True)
+    server = uvicorn.Server(config)
+    await server.serve()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
