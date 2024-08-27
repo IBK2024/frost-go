36f3d7b35f110958ddb57e8398a472f43af5559e
IBK <ibukunoluwa.adetoba@gmail.com>
2024-08-27 20:30:18 +0100

    Some changes made

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import uvicorn
 import asyncio
+import uvicorn
 from run import app
 
 
 async def main() -> None:
+    """
+    Main Code does the following:
+        - Starts the uvicorn server
+        - Runs background tasks
+    """
     config = uvicorn.Config(app, port=5000, log_level="info", reload=True)
     server = uvicorn.Server(config)
     await server.serve()
